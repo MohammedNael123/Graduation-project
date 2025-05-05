cf8039143398297f582553e22f3c210e33b68dd8
MohammedNael123 <162346777+MohammedNael123@users.noreply.github.com>
2025-05-05 15:42:01 +0300

    login fix

--- BackEnd/startserver.js
+++ BackEnd/startserver.js
@@ -7,27 +7,34 @@ const User = require("../BackEnd/Controllers/UserController");
 const logout = require("../BackEnd/Controllers/LogOutController");
 const createCourese = require("../BackEnd/Controllers/createCourse");
 const Dashboard = require("../BackEnd/Controllers/DashboardController");
-const functions = require("../BackEnd/Functions");
 const genQuiz = require('./Controllers/AiTools/Generate-quiz.js');
 const genTimeTable = require('./Controllers/AiTools/Generate-TimeTable.js');
-const deleteCourse = require('./Controllers/DeleteCourse.js');
-const testme = require('./Controllers/AiTools/testMe.js');
-const fileUpload = require('./Controllers/FileUploader.js');
+const deletecourse = require("./Controllers/DeleteCourse.js");
+const addFileToCourse = require("./Controllers/FileUploadToDropbox.js");
+const editCourse = require("./Controllers/editcourse.js")
+const generatesummary = require("./Controllers/AiTools/summarization.js");
+const testme = require("./Controllers/AiTools/testMe.js");
+const majorcheck = require("./Controllers/AiTools/major check12.js");
 
 const app = express();
+//app.use(cookieParser());
+
+app.set('trust proxy', 1); // مهم جدًا عند النشر على Render
 
 app.use(session({
-  secret:"my secret",
+  secret: "my secret",
   resave: false,
-  saveUninitialized:true,
+  saveUninitialized: true,
   cookie: {
-    secure: false,
-    }
-  }));
+    secure: true,            // لازم يكون true لأنك تستخدم HTTPS
+    sameSite: "none"         // مهم للسماح بالكوكيز عبر نطاقين مختلفين (cross-site)
+  }
+}));
+
 
 app.use(cors({
-  origin:["https://darisni.netlify.app"],
-  methods:["POST","GET"],
+  origin: ["https://darisni.netlify.app", "http://localhost:3000"],
+  methods: ["POST", "GET"],
   credentials: true
 }));
 
@@ -48,12 +55,16 @@ app.use("/" , log_in);
 app.use("/" , User);
 app.use("/" , logout);
 app.use("/" , createCourese);
-app.use("/" , fileUpload);
 app.use("/" , Dashboard);
 app.use("/" , genQuiz);
 app.use("/" , genTimeTable);
-app.use("/" , deleteCourse);
+app.use("/" , deletecourse);
+app.use("/" , addFileToCourse);
+app.use("/" , editCourse);
+app.use("/" , generatesummary);
 app.use("/" , testme);
+app.use("/" , majorcheck);
+
 
 
 
