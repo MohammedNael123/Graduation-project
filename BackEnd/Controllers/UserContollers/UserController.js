cc823ce6f31dc411751f892e9802ee3885e3b471
M7MDJABER <mohammad.altaip.jaber@gmail.com>
2025-06-10 00:16:29 +0300

    edit userController

--- BackEnd/Controllers/UserContollers/UserController.js
+++ BackEnd/Controllers/UserContollers/UserController.js
@@ -6,7 +6,6 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 router.use(express.json());
 // router.use(session({
 //   secret: "my secret",
@@ -25,7 +24,6 @@ router.get("/profile", async (req, res) => {
     name: req.session.user.name,
     role: req.session.user.role,
   });
-
 });
 
 router.post("/update-profile", async (req, res) => {
@@ -33,23 +31,39 @@ router.post("/update-profile", async (req, res) => {
     const userId = req.session.user?.id;
     const { full_name } = req.body;
     if (!userId) {
-      console.error("User Not Logged in!");
-      return res.json({ message: "NOT logged in!" });
+      console.error("User not logged in!");
+      return res.status(401).json({ message: "Not logged in!" });
     }
-    const { data, error } = supabase
+
+
+    const { data, error } = await supabase
       .from("profiles")
       .update({ full_name: full_name })
       .eq("id", userId);
 
-    if (error) {
-      console.error("Error Editing username!");
-      return res.status(500).json({ message: "Error Editing username!" });
-    }
-  } catch (err) {
-    console.error("Unexpected Error Occurred");
-    return res.status(500).json({ message: "Internal Server Error!" });
-  }
 
+    if (error) {
+      console.error("Error editing username!", error.message);
+      return res.status(500).json({ message: "Error editing username!" });
+    }
+
+    req.session.user={
+      id: req.session.user?.id,
+      name: full_name,
+      email: req.session.user?.email,
+      role: req.session.user?.role
+    };
+    req.session.save((err)=>{
+      if(err){
+      console.error("session not saved!");
+      return res.status(500).json("session save failed!");
+  }});
+    return res.status(200).json({ message: "Profile updated successfully." });
+
+  } catch (err) {
+    console.error("Unexpected error occurred", err);
+    return res.status(500).json({ message: "Internal server error!" });
+  }
 });
 
 
