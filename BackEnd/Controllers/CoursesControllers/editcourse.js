731523890dd22ae2e9a1a1eca30d3fe7302173dc
M7MDJABER <mohammad.altaip.jaber@gmail.com>
2025-06-09 18:05:33 +0300

    fix editcourse.js

--- BackEnd/Controllers/CoursesControllers/editcourse.js
+++ BackEnd/Controllers/CoursesControllers/editcourse.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const session = require("express-session");
-const {createClient} = require("@supabase/supabase-js");
+const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
 const router = express.Router();
 
+router.use(session({
+  secret: "my secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
-router.post("/editCourse", async (req, res) => {
-    const userId = req.session.user?.id;
-    const { courseId, newName } = req.body;
-  
-    if (!userId || !courseId || !newName) {
-      return res.status(400).json({ error: "Missing data." });
-    }
-  
-    const { error: errorWhileEditing } = await supabase
-      .from("UserCourses")
-      .update({ name: newName })
-      .eq("id", courseId);
-  
-    if (errorWhileEditing) {
-      return res.status(500).json("error while updating the name try again later");
-    } else {
-      return res.status(200).json("updated");
-    }
-  });
-  
-  
+router.post("/EditCourseName", async (req, res) => {
+  const userId = req.session.user?.id;
+  const { courseId, newName } = req.body;
+
+  if (!userId || !courseId || !newName) {
+    return res.status(400).json({ error: "Missing data." });
+  }
+
+  const { error } = await supabase
+    .from("UserCourses")
+    .update({ name: newName })
+    .eq("id", courseId);
+
+  if (error) {
+    return res.status(500).json("error while updating the name try again later");
+  }
+
+  return res.status(200).json("updated");
+});
 
 module.exports = router;
